cb000eb2b3d9722587acc5b4fefebc813f27c205
SIY1121 <sota.0512.0213@gmail.com>
2026-05-24 17:15:19 +0900

    cargo fmt

--- examples/echo-lama/src-plugin/src/gui/runtime.rs
+++ examples/echo-lama/src-plugin/src/gui/runtime.rs
@@ -9,9 +9,7 @@ use wrac_clap_adapter::{
     GuiConfiguration, GuiSize, HostGuiResizeRequester, HostParameterEditNotifier, PluginError,
     PluginResult,
 };
-use wrac_wxp_gui::{
-    DpiConverter, ParentWindowHandle, WxpGuiResizeHandle, WxpGuiRuntime,
-};
+use wrac_wxp_gui::{DpiConverter, ParentWindowHandle, WxpGuiResizeHandle, WxpGuiRuntime};
 use wxp::{WebContext, WxpCommandHandler, WxpWebView, WxpWebViewBuilder, dpi::LogicalSize};
 
 use crate::commands::register_commands;
@@ -38,7 +36,6 @@ pub(super) const MAX_GUI_SIZE: GuiSize = GuiSize {
     width: 396,
     height: 573,
 };
-
 
 // Embed the frontend zip only for release builds; debug builds use the Vite dev server.
 #[cfg(not(debug_assertions))]
